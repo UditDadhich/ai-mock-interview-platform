e72ca881119c09b4122d075e3143512ddead5ef8
UditDadhich <dadhichudit2@gmail.com>
2026-06-07 08:17:05 +0530

    voice to AI

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import maleVideo from "../assets/videos/male-ai.mp4";
 import femaleVideo from "../assets/videos/female-ai.mp4";
 import Timer from "./Timer.jsx";
@@ -30,7 +30,131 @@ function Step2Interview({ interviewData, onFinish }) {
     const videoRef = useRef(null);
 
     const currentQuestion = questions[currentIndex];
+
+    useEffect (() => {
+      const loadVoices = () => {
+        const voices = window.speechSynthesis.getVoices();
+        if(!voices.length) return;
+
+        // Try know female voices first
+
+        const femaleVoice =
+          voices.find(v => 
+            v.name.toLowerCase().includes("zira") ||
+            v.name.toLowerCase().includes("samantha") || 
+            v.name.toLowerCase().includes("female")
+          );
+
+          if(femaleVoice) {
+            setSelectedVoice(femaleVoice);
+            setVoiceGender("female");
+            return;
+          }
+
+          // Try know male voices
+
+          const maleVoice = 
+            voices.find(v => 
+              v.name.toLowerCase().includes("david") || 
+              v.name.toLowerCase().includes("mark") || 
+              v.name.toLowerCase().includes("male")
+            );
+
+            if(maleVoice) {
+              setSelectedVoice(maleVoice);
+              setVoiceGender("male");
+              return;
+            }
+
+            // Fallback: first voice (assume female)
+
+            setSelectedVoice(voices[0]);
+            setVoiceGender("female");
+      };
+
+      loadVoices();
+      window.speechSynthesis.onvoiceschanged = loadVoices;
+    } , [])
+
+    const videoSource = voiceGender === "male" ? maleVideo : femaleVideo;
+
+    /* ------------------------------------ SPEAK FUNCTION ---------------------------------- */
+
+    const speakText = (text) => {
+      return new Promise((resolve) => {
+        if(!window.speechSynthesis || !selectedVoice) {
+          resolve();
+          return;
+        }
+
+        window.speechSynthesis.cancel();
+
+        // Add natural pauses after commas and periods
+
+        const humanText = text
+          .replace(/,/g, ", ... ")
+          .replace(/\./g, ". ...");
+
+          const utterance = new SpeechSynthesisUtterance(humanText);
+
+          utterance.voice = selectedVoice;
+
+          utterance.rate = 0.92;
+          utterance.pitch = 1.05;
+          utterance.volume = 1;
+
+          utterance.onstart = () => {
+            setIsAIPlaying(true);
+            videoRef.current?.play();
+          }
+          utterance.onend = () => {
+            videoRef.current?.pause();
+            videoRef.current.currentTime = 0;
+            setIsAIPlaying(false);
+
+            setTimeout(() => {
+              setSubtitle("");
+              resolve();
+            }, 300);
+          };
+
+          setSubtitle(text);
+
+          window.speechSynthesis.speak(utterance);
+      })
+    }
     
+
+    useEffect(() => {
+      if(!selectedVoice){
+        return;
+      }
+      const runIntro = async ()=> {
+        if(isIntroPhase){
+          await speakText(
+            `Hi ${userName}, it's great to meet you today. I hope you're feeling 
+            confident and ready`
+          );
+
+          await speakText(
+            "I'll ask you a few questions. Just answer naturally, and take your time. Let's begin."
+          )
+          setIsIntroPhase(false)
+        }else if(currentQuestion){
+          await new Promise(r => setTimeout(r,800));
+
+          // If last question (hard level)
+
+          if(currentIndex === questions.length - 1){
+            await speakText("Alright, this one might be a bit more challenging");
+          }
+          await speakText(currentQuestion.question);
+        }
+      }
+
+      runIntro();
+
+    },[selectedVoice , isIntroPhase ,currentIndex])
 
   return (
     <div
@@ -50,7 +174,9 @@ function Step2Interview({ interviewData, onFinish }) {
         >
           <div className="w-full max-w-md rounded-2xl overflow-hidden shadow-xl">
             <video
-              src={femaleVideo}
+              src={videoSource}
+              key={videoSource}
+              ref={videoRef}
               muted
               playsInline
               preload="auto"
@@ -58,7 +184,17 @@ function Step2Interview({ interviewData, onFinish }) {
             />
           </div>
 
-          {/* {subTitle Pending} */}
+          {/* {subTitle} */}
+
+           {subtitle && (
+            <div className="w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl p-4 shadow-sm">
+              <p className="text-gray-700 text-sm sm:text-base
+              font-medium text-center leading-relaxed">
+                  {subtitle}
+              </p>
+            </div>
+          )}
+
 
           {/* {timer area} */}
 
@@ -68,9 +204,9 @@ function Step2Interview({ interviewData, onFinish }) {
           >
             <div className="flex justify-between items-center">
               <span className="text-sm text-gray-500">Interview Status</span>
-              <span className="text-sm font-semibold text-emerald-600">
-                AI Speaking
-              </span>
+              {isAIPlaying && <span className="text-sm font-semibold text-emerald-600">
+                {isAIPlaying ? "AI Speaking" : " "}
+              </span>}
             </div>
 
             <div className="h-px bg-gray-200"></div>
@@ -85,7 +221,7 @@ function Step2Interview({ interviewData, onFinish }) {
                     <span className="text-xs text-gray-400">Current Questions</span>
                 </div>
                 <div>
-                    <span className="text-2xl font-bold text-emerald-600">5</span>
+                    <span className="text-2xl font-bold text-emerald-600">{questions.length}</span>
                     <span className="text-xs text-gray-400">Total Questions</span>
                 </div>
             </div>
