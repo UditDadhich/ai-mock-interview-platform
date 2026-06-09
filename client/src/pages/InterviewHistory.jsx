61ff435f06647cdd61a0bc5279905c38d1997382
UditDadhich <dadhichudit2@gmail.com>
2026-06-09 21:28:04 +0530

    Design of history page

--- client/src/pages/InterviewHistory.jsx
+++ client/src/pages/InterviewHistory.jsx
@@ -15,7 +15,7 @@ function InterviewHistory() {
       try {
         const result = await axios.get(
           ServerUrl + "/api/interview/get-interview",
-          { withCredentials: true }
+          { withCredentials: true },
         );
 
         console.log(result.data);
@@ -28,33 +28,60 @@ function InterviewHistory() {
     getMyInterviews();
   }, []);
 
-  return(
-   <div className="min-h-screen bg-linear-to-br from-gray-50 to-emerald-50
-   py-10">
-
-    <div className="w-[90vw] lg:w-[70vw] max-w-[90%] mx-auto">
-
-        <div className="mb-10 w-full flex items-start gap-4">
-            <button
-            onClick={()=> navigate("/")}
+  return (
+    <div
+      className="min-h-screen bg-linear-to-br from-gray-50 to-emerald-50
+   py-10"
+    >
+      <div className="w-[90vw] lg:w-[70vw] max-w-[90%] mx-auto">
+        <div className="mb-10 w-full flex items-start gap-4 flex-wrap">
+          <button
+            onClick={() => navigate("/")}
             className="mt-1 p-3 rounded-full
-            bg-white shadow hover:shadow-md transition"><FaArrowLeft className="text-gray-600"/></button>
+            bg-white shadow hover:shadow-md transition"
+          >
+            <FaArrowLeft className="text-gray-600" />
+          </button>
 
-            <div>
-                <h1 className="text-3xl font-bold text-gray-800">
-                    Interview History
-                </h1>
+          <div>
+            <h1 className="text-3xl font-bold flex-nowrap text-gray-800">
+              Interview History
+            </h1>
 
-                <p className="text-gray-500 mt-2">
-                    Track your past interviews and performance reports.
-                </p>
-            </div>
-
+            <p className="text-gray-500 mt-2">
+              Track your past interviews and performance reports.
+            </p>
+          </div>
         </div>
 
+        {interviews.length === 0 ? (
+          <div className="bg-white p-10 rounded-2xl shadow text-center">
+            <p className="text-gray-500">
+              No interview found. Start your first interview.
+            </p>
+          </div>
+        ) : (
+          <div className="grid gap-6">
+            {interviews.map((item, index) => (
+              <div key={index}
+                className="bg-white p-6 rounded-2xl shadow-md hover:shadow-xl
+            transition-all duration-300 cursor-pointer border border-gray-100">
+
+                <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+
+                    <div></div>
+
+                     <div></div>
+
+                </div>
+              
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-    
-    </div>
-)}
+  );
+}
 
 export default InterviewHistory;
