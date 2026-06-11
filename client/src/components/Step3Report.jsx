494aa257c5d499492630e91364817c12872409ea
UditDadhich <dadhichudit2@gmail.com>
2026-06-11 07:35:22 +0530

    solved dependency error

--- client/src/components/Step3Report.jsx
+++ client/src/components/Step3Report.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts"
