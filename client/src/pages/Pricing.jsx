6d3832544510389446a45d2a9a549a6a0e0e0317
UditDadhich <dadhichudit2@gmail.com>
2026-06-12 21:38:56 +0530

    Change import from 'motion/react' to 'framer-motion'

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaArrowLeft, FaCheckCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";;
 import axios from "axios";
 import { ServerUrl } from "../App";
 import { useDispatch } from "react-redux";
