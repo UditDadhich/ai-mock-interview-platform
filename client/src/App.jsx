ca7ab8d9a48d1f21d4b3475aaec68c8ecaea71a4
UditDadhich <dadhichudit2@gmail.com>
2026-06-12 21:29:25 +0530

    Change ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory.jsx'
 import Pricing from './pages/Pricing.jsx'
 import InterviewReport from './pages/InterviewReport.jsx'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://ai-mock-interview-platform-s043.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
